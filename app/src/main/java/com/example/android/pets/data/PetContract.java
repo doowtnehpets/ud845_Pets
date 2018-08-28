26731c6efdf88ecf9ddcb3a0ef9b7535957f80dd
doowtnehpets <doowtnehpets@gmail.com>
2018-08-28 11:01:17 -0600

    Data - Lesson 4 - Part 10

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,13 +1,25 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
+
+    // Content URI constants
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     // Empty Constructor
     private PetContract() {
     }
 
     public static final class PetEntry implements BaseColumns {
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
         // Table name
         public final static String TABLE_NAME = "pets";
 
