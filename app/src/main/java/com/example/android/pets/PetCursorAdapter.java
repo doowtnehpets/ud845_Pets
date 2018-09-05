2d69794f09c28ffe9720bacc135c320b554176b8
doowtnehpets <doowtnehpets@gmail.com>
2018-09-05 12:33:09 -0600

    Data - Lesson 5 - Part 4

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,13 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract.PetEntry;
 
 public class PetCursorAdapter extends CursorAdapter {
 
@@ -14,17 +18,42 @@ public class PetCursorAdapter extends CursorAdapter {
      * @param context The context
      * @param c       The cursor from which to get the data
      */
-    public PetCursorAdapter(Context context, Cursor c) {
-        super(context, c);
+    public PetCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, 0);
     }
 
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        // Inflate a list item using the layout list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
+
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        // Grab the TextView for name and summary from the layout
+        TextView nameTextView = view.findViewById(R.id.name);
+        TextView summaryTextView = view.findViewById(R.id.summary);
 
+        // Find the columns of the pet attributes
+        int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+
+        // Read the attributes from the Cursor for the current pet
+        String petName = cursor.getString(nameColumnIndex);
+        String petBreed = cursor.getString(breedColumnIndex);
+
+        // Set the TextViews to the data pulled
+        nameTextView.setText(petName);
+        summaryTextView.setText(petBreed);
     }
 }
